c60a9c5705d7ff47fe34b47ad8c42d61613bcc16
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-10-11 18:03:44 -0700

    setting up navigation

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Favorites from "./screens/Favorites";
+import Categories from "./screens/Categories";
+import Meals from "./screens/Meals";
+import SpecificMeal from "./screens/SpecificMeal";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const StackNav = () => {
+		return (
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Stack"
+					component={Categories}
+					options={{
+						headerShown: false,
+					}}
+				/>
+				<Stack.Screen name="Meals" component={Meals} />
+				<Stack.Screen name="SpecificMeal" component={SpecificMeal} />
+			</Stack.Navigator>
+		);
+	};
+
+	return (
+		<>
+			<StatusBar style="auto" />
+			<NavigationContainer>
+				<Tab.Navigator>
+					<Tab.Screen
+						name="Categories"
+						component={StackNav}
+						options={
+							{
+								// headerShown: false,
+							}
+						}
+					/>
+					<Tab.Screen
+						name="Favorites"
+						component={Favorites}
+						options={{}}
+					/>
+				</Tab.Navigator>
+			</NavigationContainer>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		// flex: 1,
+		// backgroundColor: '#fff',
+		// alignItems: 'center',
+		// justifyContent: 'center',
+	},
 });
