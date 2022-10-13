769bb2aa8bc7d26ec2ed1ddd78ed5499e01ec0d8
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-10-13 16:44:59 -0700

    meals filtered based on category id

--- screens/MealsScreen.js
+++ screens/MealsScreen.js
@@ -1,14 +1,21 @@
 import { StyleSheet, View, Text } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
 import { useRoute } from "@react-navigation/native";
 
 const Meals = () => {
 	const route = useRoute();
+	const list = MEALS.filter((meal) =>
+		meal.categoryIds.includes(route.params.id)
+	);
+
+	console.log(list);
 
 	return (
 		<View>
 			<Text>{route.params.id}</Text>
 			<Text>{route.params.mealTitle}</Text>
+			<Text>{}</Text>
 		</View>
 	);
 };
