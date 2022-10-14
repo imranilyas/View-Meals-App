1ab3730b4d010d6536523cfd0bdd384a317a4724
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-10-14 12:20:36 -0700

    change title of mealscreen dynamically

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -56,11 +56,4 @@ export default function App() {
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		// flex: 1,
-		// backgroundColor: '#fff',
-		// alignItems: 'center',
-		// justifyContent: 'center',
-	},
-});
+const styles = StyleSheet.create({});
--- screens/MealsScreen.js
+++ screens/MealsScreen.js
@@ -1,21 +1,35 @@
 import { StyleSheet, View, Text } from "react-native";
 import { MEALS } from "../data/dummy-data";
 
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
 
 const Meals = () => {
 	const route = useRoute();
+	const navigate = useNavigation();
+
 	const list = MEALS.filter((meal) =>
 		meal.categoryIds.includes(route.params.id)
 	);
 
-	console.log(list);
+	// console.log(list);
+
+	useLayoutEffect(() => {
+		navigate.setOptions({
+			title: route.params.mealTitle,
+		});
+		console.log("inside useLayoutEffect");
+	}, [route, navigate]);
 
 	return (
 		<View>
 			<Text>{route.params.id}</Text>
 			<Text>{route.params.mealTitle}</Text>
-			<Text>{}</Text>
+			<Text>{list.title}</Text>
+			<Text>{list.affordability}</Text>
+			<Text>{list.complexity}</Text>
+			<Text>{list.imageUrl}</Text>
+			<Text>{list.duration}</Text>
 		</View>
 	);
 };
