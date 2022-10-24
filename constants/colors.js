2795ce1449acecebeb49037870e8394c2104faeb
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-10-24 13:26:05 -0700

    init colors for global colors file

---
+++ constants/colors.js
@@ -0,0 +1,20 @@
+const COLORS = {
+	// ICON COLOR
+	iconActive: "something",
+	iconInactive: "something",
+
+	// SHADOW COLOR
+	shadowColor: "something",
+
+	// REACT NAV HEADER
+	headerColor: "something",
+	headerFontColor: "something",
+
+	// BACKGROUND OF PAGE
+	backgroundContentColor: "something",
+
+	// FONT COLOR FOR MEAL DETAILS
+	fontColor: "something",
+};
+
+export default COLORS;
