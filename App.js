059c151af1115491d6b9aa32ce33d60a5e662295
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-10-28 10:07:27 -0700

    put store in app

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StyleSheet } from "react-native";
 import { useState } from "react";
+import { Provider } from "react-redux";
+
 import { StatusBar } from "expo-status-bar";
 import { Ionicons } from "@expo/vector-icons";
 import COLORS from "./constants/colors";
@@ -12,6 +14,7 @@ import FavoritesScreen from "./screens/FavoritesScreen";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsScreen from "./screens/MealsScreen";
 import SpecificMealScreen from "./screens/SpecificMealScreen";
+import { store } from "./redux/store";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -66,53 +69,57 @@ export default function App() {
 	return (
 		<>
 			<StatusBar style="light" />
-			<NavigationContainer>
-				<Tab.Navigator
-					sceneContainerStyle={{
-						backgroundColor: COLORS.backgroundContentColor,
-					}}
-					screenOptions={{
-						// Bottom Tab
-						tabBarStyle: { backgroundColor: COLORS.headerColor },
-						tabBarActiveTintColor: COLORS.iconActive,
-						tabBarInactiveTintColor: COLORS.iconInactive,
+			<Provider store={store}>
+				<NavigationContainer>
+					<Tab.Navigator
+						sceneContainerStyle={{
+							backgroundColor: COLORS.backgroundContentColor,
+						}}
+						screenOptions={{
+							// Bottom Tab
+							tabBarStyle: {
+								backgroundColor: COLORS.headerColor,
+							},
+							tabBarActiveTintColor: COLORS.iconActive,
+							tabBarInactiveTintColor: COLORS.iconInactive,
 
-						// Top Tab
-						headerStyle: {
-							backgroundColor: COLORS.headerColor,
-						},
-						headerTintColor: COLORS.headerFontColor,
-					}}
-				>
-					<Tab.Screen
-						name="Categories"
-						component={StackNav}
-						options={{
-							headerShown: false,
-							tabBarIcon: ({ color, size }) => (
-								<Ionicons
-									name="ios-list-outline"
-									color={color}
-									size={size}
-								/>
-							),
+							// Top Tab
+							headerStyle: {
+								backgroundColor: COLORS.headerColor,
+							},
+							headerTintColor: COLORS.headerFontColor,
 						}}
-					/>
-					<Tab.Screen
-						name="Favorites"
-						component={FavoritesScreen}
-						options={{
-							tabBarIcon: ({ color, size }) => (
-								<Ionicons
-									name="ios-star-outline"
-									color={color}
-									size={size}
-								/>
-							),
-						}}
-					/>
-				</Tab.Navigator>
-			</NavigationContainer>
+					>
+						<Tab.Screen
+							name="Categories"
+							component={StackNav}
+							options={{
+								headerShown: false,
+								tabBarIcon: ({ color, size }) => (
+									<Ionicons
+										name="ios-list-outline"
+										color={color}
+										size={size}
+									/>
+								),
+							}}
+						/>
+						<Tab.Screen
+							name="Favorites"
+							component={FavoritesScreen}
+							options={{
+								tabBarIcon: ({ color, size }) => (
+									<Ionicons
+										name="ios-star-outline"
+										color={color}
+										size={size}
+									/>
+								),
+							}}
+						/>
+					</Tab.Navigator>
+				</NavigationContainer>
+			</Provider>
 		</>
 	);
 }
